d5899a8ab0e1eac15e4841444959c5c4987e0d22
Uday Gadhavi <gadhaviud@gmail.com>
2025-10-04 16:10:36 +0530

    Trying to merge

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createUser, getUsers } = require('../controllers/userController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.route('/').post(protect, admin, createUser).get(protect, admin, getUsers);
-
-module.exports = router;
